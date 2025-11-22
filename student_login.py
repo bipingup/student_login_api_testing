cfbc04cd69af31aa53173bb321e9db58be00d856
bipingup <biping9930@gmail.com>
2025-11-22 13:07:49 +0530

    updated code

--- student_login.py
+++ student_login.py
@@ -26,3 +26,37 @@ async def student_data(data:student_login):
     result = await euroon_data.insert_one(data.model_dump())
     return {"message":"Data inserted Successfully"}
 
+
+def euron_helper(doc):
+    doc["iid"] = str(doc["_id"])
+    del doc["_id"]
+    return doc
+
+
+@app.get("/students/data")
+async def get_data():
+    iterms = []
+    cursor = euroon_data.find({})
+    async for document in cursor:
+        iterms.append(euron_helper(document))
+    return iterms
+
+
+
+
+@app.delete("/students/delete/{id}")
+async def delete_student_data(id: int):
+    result = await euroon_data.delete_one({"id": id})
+    
+    if result.deleted_count == 1:
+        return {"message": "Data deleted Successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Data not found")
+        
+@app.post("/students/update/{id}")
+async def update_student_data(id: int,data:student_login):
+    result = await euroon_data.update_one({"id": id}, {"$set": data.model_dump()})
+    if result.modified_count == 1:
+        return {"message": "Data updated Successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Data not found")
